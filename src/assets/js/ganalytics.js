4538f47ce5068d4f0923a7114815249bd80ac03b
fodekar <chancelvyfodekar@gmail.com>
2019-07-10 01:49:38 +0200

    [MPC-3] integrated of google analytics stats, test after remove docker build

--- src/assets/js/ganalytics.js
+++ src/assets/js/ganalytics.js
@@ -2,4 +2,4 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
-gtag('config', 'UA-143348607-1');
+gtag('config', 'UA-143348607-1'); 
